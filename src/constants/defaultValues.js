1f7beaf24788c39bd28ba35c5637a13b176a9211
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-02-12 15:35:27 +0700

    aa

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -43,7 +43,7 @@ const dev = {
         name: "bebe",
         endpoint:
           process.env.REACT_APP_STAGE === "dev"
-            ? "https://api.planforfit.com/preem_bettershape"
+            ? "https://api.planforfit.com/fit_carrot"
             : "http://localhost:3003",
         region: "ap-southeast-1",
       },
@@ -68,7 +68,7 @@ const prod = {
     endpoints: [
       {
         name: "bebe",
-        endpoint: "https://api.planforfit.com/preem_bettershape",
+        endpoint: "https://api.planforfit.com/fit_carrot",
         region: "ap-southeast-1",
       },
     ],
